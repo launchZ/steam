2ec16efe919b302e7367a25c412ec6531078bd65
robng <robinbnger@gmail.com>
2024-06-11 11:23:10 +0200

    refactor: Move `query_*` functions into parent module

--- src/a2s/mod.rs
+++ src/a2s/mod.rs
@@ -1,1 +1,44 @@
+use crate::a2s::packet::Packet;
+use std::error::Error;
+use std::net::{ToSocketAddrs, UdpSocket};
+
 pub mod packet;
+
+pub fn query_info<A: ToSocketAddrs>(socket: &UdpSocket, addr: A) -> Result<Packet, Box<dyn Error>> {
+    packet::send_a2s_packet(
+        &socket,
+        addr,
+        &[
+            0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E,
+            0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00,
+        ],
+        &[],
+        0,
+    )
+}
+
+pub fn query_rules<A: ToSocketAddrs>(
+    socket: &UdpSocket,
+    addr: A,
+) -> Result<Packet, Box<dyn Error>> {
+    packet::send_a2s_packet(
+        &socket,
+        addr,
+        &[0xFF, 0xFF, 0xFF, 0xFF, 0x56],
+        &[0xFF, 0xFF, 0xFF, 0xFF],
+        0,
+    )
+}
+
+pub fn query_players<A: ToSocketAddrs>(
+    socket: &UdpSocket,
+    addr: A,
+) -> Result<Packet, Box<dyn Error>> {
+    packet::send_a2s_packet(
+        &socket,
+        addr,
+        &[0xFF, 0xFF, 0xFF, 0xFF, 0x55],
+        &[0xFF, 0xFF, 0xFF, 0xFF],
+        0,
+    )
+}
--- src/a2s/packet/mod.rs
+++ src/a2s/packet/mod.rs
@@ -17,45 +17,6 @@ use std::io::{BufRead, Seek, SeekFrom};
 use std::net::ToSocketAddrs;
 use std::net::UdpSocket;
 
-pub fn query_info<A: ToSocketAddrs>(socket: &UdpSocket, addr: A) -> Result<Packet, Box<dyn Error>> {
-    send_a2s_packet(
-        &socket,
-        addr,
-        &[
-            0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E,
-            0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00,
-        ],
-        &[],
-        0,
-    )
-}
-
-pub fn query_rules<A: ToSocketAddrs>(
-    socket: &UdpSocket,
-    addr: A,
-) -> Result<Packet, Box<dyn Error>> {
-    send_a2s_packet(
-        &socket,
-        addr,
-        &[0xFF, 0xFF, 0xFF, 0xFF, 0x56],
-        &[0xFF, 0xFF, 0xFF, 0xFF],
-        0,
-    )
-}
-
-pub fn query_players<A: ToSocketAddrs>(
-    socket: &UdpSocket,
-    addr: A,
-) -> Result<Packet, Box<dyn Error>> {
-    send_a2s_packet(
-        &socket,
-        addr,
-        &[0xFF, 0xFF, 0xFF, 0xFF, 0x55],
-        &[0xFF, 0xFF, 0xFF, 0xFF],
-        0,
-    )
-}
-
 pub enum Packet {
     Unknown(PacketBytes),
     Info(Packet_Info),
@@ -185,7 +146,7 @@ impl RecvPacketBytes for UdpSocket {
     }
 }
 
-fn send_a2s_packet<A: ToSocketAddrs>(
+pub(crate) fn send_a2s_packet<A: ToSocketAddrs>(
     socket: &UdpSocket,
     addr: A,
     packet: &[u8],
